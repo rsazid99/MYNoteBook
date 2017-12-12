c8135b88f6dc657cf6d16facc87dc087c0e5142e
rsazid99 <rsazid99@gmail.com>
2017-12-12 19:05:43 +0600

    add

--- DataStructure/BIT(Fenwick).cpp
+++ DataStructure/BIT(Fenwick).cpp
@@ -1,18 +1,18 @@
-struct fenwick{
-        int tree[maxn];
-        void update(int idx, int val, int n){
-                while(idx <= n){
-                        tree[idx] += val;
-                        idx += idx & (-idx);
-                }
+int tree[maxn];
+
+void update(int idx, int val){
+        while(idx <= n){
+                tree[idx] += val;
+                idx += idx & (-idx);
         }
-        int query(int idx)
-        {
-                int sum = 0;
-                while(idx > 0){
-                        sum += tree[idx];
-                        idx -= idx & (-idx);
-                }
-                return sum;
+}
+
+int query(int idx)
+{
+        int sum = 0;
+        while(idx > 0){
+                sum += tree[idx];
+                idx -= idx & (-idx);
         }
-};
+        return sum;
+}
